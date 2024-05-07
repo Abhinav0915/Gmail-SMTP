53deab6760c145472c7874c532eed5bee2ee7569
Abhinav Saxena <abhinav150601@gmail.com>
2024-05-07 15:26:19 +0530

    fix

--- EmailService-Backend/src/main/java/com/example/EmailServiceBackend/controller/EmailController.java
+++ EmailService-Backend/src/main/java/com/example/EmailServiceBackend/controller/EmailController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//7311115544
+
 @RestController
 @CrossOrigin
 public class EmailController {
